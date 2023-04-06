712378e5b28bc8f6f51bc2078f885218aee6d067
gabrielgiroe1 <gabriel.giroe@gmail.com>
2023-04-06 21:11:32 +0300

    Test number of mines

--- spec/factories/board.rb
+++ spec/factories/board.rb
@@ -2,8 +2,9 @@ FactoryBot.define do
   factory :board, class: 'Board' do
     board_name { "MyBoard" }
     email { "board@example.com" }
-    width { 10 }
-    height { 10 }
+    width { 3 }
+    height { 3 }
     num_mines { 5 }
+    board_data { "[[1, 1, 0], [0, 1, 0], [0, 1, 1]]"}
   end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 require 'factory_bot'
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   it "is valid with valid attributes" do
     board = build(:board)
     expect(board).to be_valid
@@ -20,5 +19,10 @@ RSpec.describe Board, type: :model do
   it "is not valid with an invalid email" do
     board = build(:board, email: "invalid_email")
     expect(board).to_not be_valid
+  end
+
+  it "generates a board with the correct number of mines" do
+    board = create(:board)
+    expect(JSON.parse(board.board_data).flatten.count(1)).to eq 5
   end
 end
